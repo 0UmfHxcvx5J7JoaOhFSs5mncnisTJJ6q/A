b532aa38ca464c6bc8fd5d09983f7dea9c4b8547
Michaja Pehl <pehl@pik-potsdam.de>
2024-12-10 15:43:21 +0100

    foo

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname)
+{
+    madrat::madratAttach(pkgname)
+}
+
+calcAlpha <- function()
+{
+    calcOutput('Omega')
+}
+
+foo <- function(n = 0) sys.call(-n)
+
+bar <- function() foo(1)
